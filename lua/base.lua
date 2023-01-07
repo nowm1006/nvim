7a8eeeecc077d7f39892fb2c34369a9f8255ceef
nowm1006 <nowm1006@yahoo.co.jp>
2023-01-07 15:42:40 +0900

    added all lua files

---
+++ lua/base.lua
@@ -0,0 +1,8 @@
+vim.cmd("autocmd!")
+vim.scriptencoding = "utf-8"
+vim.wo.number = true
+
+-- Open hoge file
+vim.api.nvim_create_user_command("Hoge", function(opts)
+	vim.cmd("e" .. "~/readme.md")
+end, {})
