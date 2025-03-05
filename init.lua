563a80ed1f5f606e65ea195cc436aa7d5baba6f6
nowm1006 <nowm1006@yahoo.co.jp>
2025-03-05 21:25:27 +0900

    save

--- init.lua
+++ init.lua
@@ -1,6 +1,64 @@
-require("base")
-require("autocmds")
-require("options")
 require("keymaps")
-require("colorscheme")
-require("plugins")
+
+-- lazy.nvimのインストール
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  }
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- lazy.nvimを使ってプラグインをインストール
+require("lazy").setup({
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  {"nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  }}
+}, {})
+
+-- LSPサーバアタッチ時の処理
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ctx)
+    local set = vim.keymap.set
+    set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { buffer = true })
+    set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = true })
+    set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { buffer = true })
+    set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { buffer = true })
+    set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = true })
+    set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", { buffer = true })
+    set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", { buffer = true })
+    set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", { buffer = true })
+    set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { buffer = true })
+    set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { buffer = true })
+    set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = true })
+    set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { buffer = true })
+    set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", { buffer = true })
+    set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", { buffer = true })
+    set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", { buffer = true })
+    set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", { buffer = true })
+    set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { buffer = true })
+  end,
+})
+
+-- プラグインの設定
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function(server_name)
+    require("lspconfig")[server_name].setup {}
+  end,
+}
+
