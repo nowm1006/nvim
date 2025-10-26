c29c8371e5ef80bcab5cf651a76e2c15e826a0da
Naoto Maeda <nowm1006@yahoo.co.jp>
2025-10-26 14:45:22 +0900

    update lsp setting for rust

--- after/lsp/rust_analyzer.lua
+++ after/lsp/rust_analyzer.lua
@@ -1,4 +1,13 @@
+local opts = {}
+local success, req_opts = pcall(require, "plugins.lsp.servers." .. "rust-analyzer")
+if success then
+  opts = req_opts
+end
+
+opts.capabilities = require("blink.cmp").get_lsp_capabilities(opts.capabilities)
+
 return {
+    opts,
     settings = {
          ["rust-analyzer"] =  {
             check = {
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,19 @@
 return {
   'saghen/blink.cmp',
+  dependencies = {
+    "rafamadriz/friendly-snippets", -- スニペット集
+    "L3MON4D3/LuaSnip", -- スニペットエンジン
+  },
   version = '*',
   build = 'cargo build --release',
   event = { "InsertEnter", "CmdLineEnter" },
-  opts = {},
+  opts = {
+    snippets = { preset = "luasnip" },
+    completion = {
+      menu = { border = 'single' },
+      documentation = { window = { border = 'single' } },
+    },
+  signature = { window = { border = 'single' } },
+  },
+  opts_extend = { "sources.default" },
 }
